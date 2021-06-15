e67b05f82bdc7c67b4d7f54a672ddde13ad6ce5f
DESKTOP-0LQGB3L\LUV_P <bsy508@naver.com>
2021-06-15 22:38:35 +0900

    2

--- router/main.js
+++ router/main.js
@@ -4,8 +4,32 @@ module.exports = function(app)
         res.render('index.html');//localhost:3000하면 실행되는 것(=index.html).
     });
 
+    app.get('/attend',function(req,res){
+        res.render('attendance.html');
+    });
+
+    app.get('/contract',function(req,res){
+        res.render('contract.html');
+    });
+
+    app.get('/mgr',function(req,res){
+        res.render('manager.html');
+    });
+
+    app.get('/noti',function(req,res){
+        res.render('notification.html');
+    });
+
+    app.get('/site-show',function(req,res){
+        res.render('site_show.html');
+    });
+
     app.get('/site-register',function(req,res){
         res.render('site_registration.html');
+    });
+
+    app.get('/token',function(req,res){
+        res.render('token.html');
     });
 	
 	
